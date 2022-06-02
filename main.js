4e65706c1cc3d3ab70f1fdbccacb32264b6b0aba
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-06-02 17:09:53 +0200

    wettervorhersagen mit popup

--- main.js
+++ main.js
@@ -57,13 +57,25 @@ async function loadWind(url) {
     layerControl.addOverlay(overlays.wind, `ECMWF Windvorhersage für ${forecastLabel}`)
 
     L.velocityLayer({
-        data:jsondata
+        data:jsondata,
+        lineWidth: 2,
+        displayOptions:{
+            velocityType:"",
+            
+        }
     }).addTo(overlays.wind);
 };
 loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europe.json");
 
 // Wettervorhersage
 async function loadWeather(url) {
+const response =await fetch(url);
+const jsondata = await response.json();
 
+layerControl.addOverlay(overlays.weather, "Wettervorhersage met. no");
+
+let marker = L.circleMarker([
+    47.26722, 11.392778
+]).bindPopup("Wettervorhersage").addTo(overlays.weather)
 };
 loadWeather("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
