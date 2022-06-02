b8084e502b5ceb15f894e5f75f9d6ba080c07c31
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-06-02 18:27:26 +0200

    label für wettericons noch hinzu

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ async function loadWeather(url) {
             <li>Niederschlag: ${details.precipitation_amount} (mm)</li>
             <li>Relative Luftfeuchtigkeit: ${details.relative_humidity} (%)</li>
             <li>Windrichtung: ${details.wind_from_direction} (°)</li>
-            <li>Windgeschwindigkeit: ${details.wind_speed * 3.6} (km/h)</li>
+            <li>Windgeschwindigkeit: ${(details.wind_speed * 3.6). toFixed (1)}  (km/h)</li>
         </ul>
     `;
 
@@ -109,8 +109,11 @@ async function loadWeather(url) {
 //Weathericon
 for (let i=0; i <=24; i+= 3) {
     let symbol = jsondata.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
-    popup += `<img src="icons/${symbol}.svg" alt="${symbol}" style="width: 32px">`;
-        
+      
+    let forecastDate = new Date(jsondata.properties.timeseries[i].time);
+    let forecastLabel = formatDate(forecastDate);
+
+    popup += `<img src="icons/${symbol}.svg" title="${forecastLabel}" alt= ${symbol}" style="width: 32px">`;
 };
 
 
